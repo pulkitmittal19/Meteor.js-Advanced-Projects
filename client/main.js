ce2d35b9f111470a6d29ac39b96ea835c1caa52e
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-04-11 20:39:50 +0530

    added Mongodb and UI improved

--- client/main.js
+++ client/main.js
@@ -1,8 +1,49 @@
 import { Template } from 'meteor/templating';
-
 import './main.html';
+import { Notes } from '../lib/collections.js';
 
-Template.note.helpers({
-  notes: [
-    {text: "My Note 1"}]
+Template.body .helpers({
+  /*notes: [
+    {text: "My Note 1"},
+    {text: "My Note 2"},
+    {text: "My Note 3"}
+  
+  
+  ]
+ */
+  notes(){
+    return Notes.find({});
+  }
+});
+
+
+
+Template.add.events({
+  "submit .add-form": function(){
+    event.preventDefault();
+
+    const target = event.target;
+    const text= target.text.value;
+
+
+    Notes.insert({
+      text:text,
+      createdAt: new Date()
+    });
+
+
+    target.text.value=" ";
+
+    $('#addModal').modal("close");
+    return false;
+  }
+});
+
+Template.note.events({
+  "click .delete-note" : function(){
+    Notes.remove(this._id);
+
+    return false;
+
+  }
 })
