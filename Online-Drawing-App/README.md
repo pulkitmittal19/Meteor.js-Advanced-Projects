7889c7d2af4b0c1b97807fbed128367cdf80fcdb
pulkitmittal19 <pulkitmittal19@gmail.com>
2020-05-08 14:42:59 +0530

    Add:initial files

---
+++ Online-Drawing-App/README.md
@@ -0,0 +1,18 @@
+# Online Drawing app-
+
+Full stack web-application using Meteor.js
+
+
+
+# Project Specifications - 
+
+1) Full screen drawing app
+2) Different option for brush colors
+3) Option to change brush thickness
+
+
+# Running the project-
+
+1) Clone this repository
+2) run the command 'meteor' using terminal
+
---
+++ Online-Drawing-App/client/drawingApp.js
@@ -0,0 +1,81 @@
+points = new Meteor.Collection('pointsCollection');
+var canvas;
+
+// we use these for drawing more interesting shapes
+var lastX=0;
+var lastY=0;
+var strokeWidth = 1;
+var thickness=1;
+var strokeColor = "black";
+
+Meteor.startup( function() {
+  canvas = new Canvas();
+
+  Deps.autorun( function() {
+    var data = points.find({}).fetch();
+
+    if (canvas) {
+      canvas.draw(data);
+    }
+  });
+});
+
+Template.wall.events({
+
+  "click button.clear": function (event) {
+    Meteor.call('clear', function() {
+      canvas.clear();
+    });
+  },
+
+  
+})
+
+var markPoint = function() {
+
+  var offset = $('#canvas').offset();
+
+
+
+      if (lastX==0) {
+        lastX = (event.pageX - offset.left);
+        lastY = (event.pageY - offset.top);
+      }
+      points.insert({
+        
+        //2) draw a line - requires you to change the code in drawing.js
+        x: (event.pageX - offset.left),
+        y: (event.pageY - offset.top),
+        x1: lastX,
+        y1: lastY,
+        
+        w: thickness,
+        
+        c: strokeColor,
+
+
+      }); // end of points.insert()
+
+        lastX = (event.pageX - offset.left);
+        lastY = (event.pageY - offset.top);
+
+}
+
+Template.canvas.events({
+  'click': function (event) {
+    markPoint();
+  },
+  'mousedown': function (event) {
+    Session.set('draw', true);
+  },
+  'mouseup': function (event) {
+    Session.set('draw', false);
+    lastX=0;
+    lasyY=0;
+  },
+  'mousemove': function (event) {
+    if (Session.get('draw')) {
+      markPoint();
+    }
+  }
+});
